f76f86e4cc64c60db3b823055a7ea707d44cdc7f
lisianegrzygorczyk <140643314+lisianegrzygorczyk@users.noreply.github.com>
2025-10-22 08:12:21 -0300

    aaaa

--- java/script.js
+++ java/script.js
@@ -6,3 +6,9 @@ const textoResultados = document.querySelector(".texto-resultado");
 const BotaojogarNovamente = document.querySelector(".novamente-botao");
 const telaInicial = document.querySelector(".tela-inicial");
 const botaoIniciar = document.querySelector(".iniciar-botao");
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+botaoIniciar.addEventListener('click,iniciajogo');
