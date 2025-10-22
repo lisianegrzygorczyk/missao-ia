6160ed84b28a4a36753992147ecc893eaa84cf93
lisianegrzygorczyk <140643314+lisianegrzygorczyk@users.noreply.github.com>
2025-10-22 14:28:15 -0300

    gggg

--- java/script.js
+++ java/script.js
@@ -1,6 +1,6 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativs");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultados = document.querySelector(".caixa-resultados");
 const textoResultados = document.querySelector(".texto-resultado");
 const BotaojogarNovamente = document.querySelector(".novamente-botao");
@@ -12,3 +12,33 @@ let perguntaAtual;
 let historiaFinal = "";
 
 botaoIniciar.addEventListener('click,iniciajogo');
+function iniciaJogo (){
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.style.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultados.classList.remove("mostrar");
+    mostraPergunta();
+}
+
+function mostraPergunta (){
+    if (atual >= Perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativas of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativas.texto;
+        botaoAlternativas.addEventListener("click, "() => respostaSelecionada(alternativa));
+caixaAlternativas.appendChild(botaoAlternativas);
+
+    }
+}
