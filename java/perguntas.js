f86e1d3f3056a404c306ce17f88141ee30d0cc5f
lisianegrzygorczyk <140643314+lisianegrzygorczyk@users.noreply.github.com>
2025-11-03 09:49:51 -0300

    aaa

---
+++ java/perguntas.js
@@ -0,0 +1,7 @@
+export const perguntas = [
+    {
+    enunciado:"A Rede Distribuidora Global" implementará IA e automação, tornando 500 empregos obsoletos nos próximos dois anos. A empresa precisa escolher entre priorizar o lucro ou o bem-estar social.",
+    alternativas:]
+    }
+    texto: "Requalificação (Reskilling) para funções técnicas (ex: manutenção de IA, análise de dados) com salários 30% maiores.",
+
