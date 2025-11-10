7fcb669502afccfaa716cb6a0bf5dd5da16f8337
lisianegrzygorczyk <140643314+lisianegrzygorczyk@users.noreply.github.com>
2025-11-10 09:51:11 -0300

    ffff

--- java/perguntas.js
+++ java/perguntas.js
@@ -10,26 +10,31 @@ export const perguntas = [
             proxima: 1,
        },
     {
-            texto: ""
-            afirmaçao [: ""
+            texto: "A empresa decide que a maior utilidade será alcançada investindo no capital humano para elevar o nível de toda a comunidade."
+            afirmaçao [: "Lançamento de um grande Plano de Transição de Competências, realocando a maior parte dos recursos economizados com a automação para oferecer formação técnica avançada (reskilling) aos 500 funcionários. O objetivo é transformá-los em técnicos de manutenção de IA, analistas de dados de supply chain e especialistas em otimização de sistemas, funções que remuneram 30% a mais que os postos anteriores."
              ],
         proxima: 2,
     }
 ]
     },
     {
-        enunciado:
+        enunciado: "A empresa multinacional Logística Ágil S.A planeja a implementação de automação robótica e Inteligência Artificial, resultando na obsolescência das tarefas de cerca de 500 funcionários (60% da força operacional) nos próximos cinco anos."
         alternativas:[
             {
-                texto: ""
+                texto: "Investir em reskilling para realocar 500 funcionários em funções de maior valor, garantindo a estabilidade e elevando o nível técnico."
                 afirmacao:[
+                    "é a única estratégia que alinha o imperativo de eficiência tecnológica com a responsabilidade social de longo prazo, pois trata a automação como um investimento no capital humano e não como um motivo para corte de custos."
 
                 ],
                 proxima:3,
-            },
-            {
-            texto 
-        ]
-        
-    }
+               },
+        {
+            texto:"Priorizar o lucro rápido por meio de desligamento em massa, transferindo o ônus da recolocação para os indivíduos e o mercado."
+            afirmacao:[
+"é a única abordagem economicamente pragmática e fiduciariamente responsável, pois a função primária da 'Logística Ágil S.A.' é maximizar o valor para o acionista. O investimento em reskilling para 500 funcionários (Alternativa A) é um custo proibitivo e de alto risco, com poucas garantias de que os colaboradores, com perfil operacional, conseguirão assimilar as complexas habilidades de IA"
+            ],
+            proxima: 4,
+        }
+    ]
+},
 ]
