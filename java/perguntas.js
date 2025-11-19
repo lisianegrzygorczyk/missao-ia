c629d99ed01cb1b8b1005fe33bf63f50f838d9b7
lisianegrzygorczyk <140643314+lisianegrzygorczyk@users.noreply.github.com>
2025-11-19 08:05:18 -0300

    jhhhh

--- java/perguntas.js
+++ java/perguntas.js
@@ -1,56 +1,57 @@
 export const perguntas = [
     {
-    enunciado:"A "Logística Ágil S.A." planeja implementar automação robótica que pode substituir 60% (cerca de 500) de seus funcionários operacionais. O dilema é definir se a empresa tratará a automação como uma ameaça (foco na redução de custos e desligamento) ou uma oportunidade (foco na requalificação e crescimento).Para maximizar o benefício geral e garantir a melhor transição possível, qual estratégia a "Rede Distribuidora Global" deve adotar?",
+    enunciado:"A Logística Ágil S.A. planeja implementar automação robótica que pode substituir 60% (cerca de 500) de seus funcionários operacionais. O dilema é definir se a empresa tratará a automação como uma ameaça (foco na redução de custos e desligamento) ou uma oportunidade (foco na requalificação e crescimento).Para maximizar o benefício geral e garantir a melhor transição possível, qual estratégia a Rede Distribuidora Global deve adotar?",
     alternativas:[
         {
             texto: "Realocação em novas funções de maior valor (ex: supervisão robótica e análise de dados).",
             afirmacao:[
-                "Criação de 150 novos empregos, elevação da renda e do nível técnico regional."
+                "Criação de 150 novos empregos, elevação da renda e do nível técnico regional.",
             ],
             proxima: 1,
        },
     {
-            texto: "A empresa decide que a maior utilidade será alcançada investindo no capital humano para elevar o nível de toda a comunidade."
-            afirmaçao [: "Lançamento de um grande Plano de Transição de Competências, realocando a maior parte dos recursos economizados com a automação para oferecer formação técnica avançada (reskilling) aos 500 funcionários. O objetivo é transformá-los em técnicos de manutenção de IA, analistas de dados de supply chain e especialistas em otimização de sistemas, funções que remuneram 30% a mais que os postos anteriores."
+            texto: "A empresa decide que a maior utilidade será alcançada investindo no capital humano para elevar o nível de toda a comunidade.",
+            afirmaçao:[
+                "Lançamento de um grande Plano de Transição de Competências, realocando a maior parte dos recursos economizados com a automação para oferecer formação técnica avançada (reskilling) aos 500 funcionários. O objetivo é transformá-los em técnicos de manutenção de IA, analistas de dados de supply chain e especialistas em otimização de sistemas, funções que remuneram 30% a mais que os postos anteriores.",
              ],
         proxima: 2,
-    }
-]
+    } 
+      ]
     },
     {
-        enunciado: "A empresa multinacional Logística Ágil S.A planeja a implementação de automação robótica e Inteligência Artificial, resultando na obsolescência das tarefas de cerca de 500 funcionários (60% da força operacional) nos próximos cinco anos."
+        enunciado: "A empresa multinacional Logística Ágil S.A planeja a implementação de automação robótica e Inteligência Artificial, resultando na obsolescência das tarefas de cerca de 500 funcionários (60% da força operacional) nos próximos cinco anos.",
         alternativas:[
             {
-                texto: "Investir em reskilling para realocar 500 funcionários em funções de maior valor, garantindo a estabilidade e elevando o nível técnico."
+                texto: "Investir em reskilling para realocar 500 funcionários em funções de maior valor, garantindo a estabilidade e elevando o nível técnico.",
                 afirmacao:[
-                    "é a única estratégia que alinha o imperativo de eficiência tecnológica com a responsabilidade social de longo prazo, pois trata a automação como um investimento no capital humano e não como um motivo para corte de custos."
-
-                ],
+                    "é a única estratégia que alinha o imperativo de eficiência tecnológica com a responsabilidade social de longo prazo, pois trata a automação como um investimento no capital humano e não como um motivo para corte de custos.",
+             ],
                 proxima:3,
                },
         {
-            texto:"Priorizar o lucro rápido por meio de desligamento em massa, transferindo o ônus da recolocação para os indivíduos e o mercado."
+            texto:"Priorizar o lucro rápido por meio de desligamento em massa, transferindo o ônus da recolocação para os indivíduos e o mercado.",
             afirmacao:[
-"é a única abordagem economicamente pragmática e fiduciariamente responsável, pois a função primária da 'Logística Ágil S.A.' é maximizar o valor para o acionista. O investimento em reskilling para 500 funcionários (Alternativa A) é um custo proibitivo e de alto risco, com poucas garantias de que os colaboradores, com perfil operacional, conseguirão assimilar as complexas habilidades de IA"
+              "E a única abordagem economicamente pragmática e fiduciariamente responsável, pois a função primária da 'Logística Ágil S.A.' é maximizar o valor para o acionista. O investimento em reskilling para 500 funcionários (Alternativa A) é um custo proibitivo e de alto risco, com poucas garantias de que os colaboradores, com perfil operacional, conseguirão assimilar as complexas habilidades de IA",
             ],
               proxima: 4,
         }
     ]
-},
+       },
 {
-    enunciado:
+    enunciado: "A empresa Logística Ágil S.A. está implementando automação e IA que substituirão as tarefas de 500 funcionários 60% da operação. O sucesso da automação garantirá uma explosão de lucros e uma eficiência inédita.",
     alternativas:[
         {
-            texto:
+            texto: "A empresa decide que o sucesso da automação deve beneficiar todos que contribuíram, incluindo os demitidos, Os funcionários remanescentes recebem um aumento salarial progressivo e bônus de performance baseados nas novas métricas de eficiência da IA, criando um vínculo direto entre a produtividade da máquina e a remuneração humana.",
             afirmacao:[
+                "O ganho de produtividade da IA é considerado riqueza social; ao conceder stock options e bônus progressivos, a empresa alinha o sucesso da máquina ao interesse de ex-funcionários e remanescentes, promovendo a estabilidade social e o engajamento tecnológico.",
 
             ],
             proxima:3,
         },
         {
-            texto:
+            texto: "A Logística Ágil S.A. implementa a automação de forma agressiva. Os lucros recordes são distribuídos quase integralmente como dividendos para os acionistas e como bônus restritos à alta cúpula que supervisionou a implementação da IA. Os 500 funcionários são desligados com indenização mínima.",
             afirmacao:[
-
+                "O lucro da automação é um benefício privado e exclusivo dos acionistas e da alta gerência; ao negar a participação nos lucros aos funcionários remanescentes, a empresa acentua a polarização salarial e a desconfiança, minando a moral e a coesão interna.",
             ],
             proxima: 4,
         }
